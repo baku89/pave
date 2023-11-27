9c6da9f0fea0b43769fd3115f1597c68e62d571a
Baku Hashimoto <mail@baku89.com>
2023-11-27 19:55:54 +0900

    Add CubicBezier and Path types

---
+++ src/CubicBezier.ts
@@ -0,0 +1,8 @@
+import {vec2} from 'linearly'
+
+export type CubicBezier = [
+	start: vec2,
+	control1: vec2,
+	control2: vec2,
+	end: vec2,
+]
---
+++ src/Path.ts
@@ -0,0 +1,38 @@
+import {vec2} from 'linearly'
+
+// Line commands
+export type CommandM = ['M', end: vec2]
+export type CommandL = ['L', end: vec2]
+export type CommandH = ['H', end: number]
+export type CommandV = ['V', end: number]
+
+// Curve commands
+export type CommandC = ['C', control1: vec2, control2: vec2, end: vec2]
+export type CommandS = ['S', control2: vec2, end: vec2]
+export type CommandQ = ['Q', control: vec2, end: vec2]
+export type CommandT = ['T', end: vec2]
+export type CommandZ = ['Z']
+
+// Arc commands
+export type CommandA = [
+	'A',
+	r: vec2,
+	rotation: number,
+	largeArc: boolean,
+	sweep: boolean,
+	end: vec2,
+]
+
+export type Command =
+	| CommandM
+	| CommandL
+	| CommandH
+	| CommandV
+	| CommandC
+	| CommandS
+	| CommandQ
+	| CommandT
+	| CommandZ
+	| CommandA
+
+export type Path = Command[]
