8375e9fb609a269d8fda6b8e29df0d811c1f18d5
Baku Hashimoto <mail@baku89.com>
2023-11-28 20:17:58 +0900

    Add memoize function to utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,22 @@ export function toFixedSimple(a: number, fractionDigits = 2): string {
 		.replace(/(?:\.0+$)|(?:(\.[0-9]*?)0+$)/, '$1')
 		.replace(/^(-?)0\./, '$1.')
 }
+
+/**
+ * Memoizes a function that takes a single object argument, using a WeakMap.
+ */
+export function memoize<Arg extends object, ReturnType>(
+	f: (arg: Arg) => ReturnType
+): (arg: Arg) => ReturnType {
+	const cache = new WeakMap<Arg, ReturnType>()
+
+	return arg => {
+		if (cache.has(arg)) {
+			return cache.get(arg)!
+		} else {
+			const result = f(arg)
+			cache.set(arg, result)
+			return result
+		}
+	}
+}
