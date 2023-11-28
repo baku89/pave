6a6a6867da1378a867afb0d0a00ff3c087bf5576
Baku Hashimoto <mail@baku89.com>
2023-11-28 15:35:08 +0900

    Add toFixedSimple

---
+++ src/utils.test.ts
@@ -0,0 +1,15 @@
+import {toFixedSimple} from './utils'
+
+describe('toFixedSimple', () => {
+	it('should work', () => {
+		expect(toFixedSimple(0.0, 2)).toEqual('0')
+		expect(toFixedSimple(0.000001, 2)).toEqual('0')
+		expect(toFixedSimple(0.1, 0)).toEqual('0')
+		expect(toFixedSimple(0.6, 0)).toEqual('1')
+		expect(toFixedSimple(-0, 2)).toEqual('0')
+		expect(toFixedSimple(0.1, 2)).toEqual('.1')
+		expect(toFixedSimple(-0.1, 2)).toEqual('-.1')
+		expect(toFixedSimple(40000.25, 2)).toEqual('40000.25')
+		expect(toFixedSimple(0.019, 2)).toEqual('.02')
+	})
+})
---
+++ src/utils.ts
@@ -0,0 +1,6 @@
+export function toFixedSimple(a: number, fractionDigits = 2): string {
+	return a
+		.toFixed(fractionDigits)
+		.replace(/(?:\.0+$)|(?:(\.[0-9]*?)0+$)/, '$1')
+		.replace(/^(-?)0\./, '$1.')
+}
