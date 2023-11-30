dc7b9647121ea6bdc17c7cbad2130dd00cb8d8bf
Baku Hashimoto <mail@baku89.com>
2023-11-30 13:19:37 +0900

    Merge d.ts

--- demo/modules.d.ts
+++ demo/modules.d.ts
@@ -2,3 +2,8 @@ declare module 'safer-eval' {
 	function saferEval(code: string, context?: object): void
 	export = saferEval
 }
+
+declare module '*?raw' {
+	const content: string
+	export default content
+}
