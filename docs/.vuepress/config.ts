eb2447d9ab91efcb3368d60fe09c325a65263b76
Baku Hashimoto <mail@baku89.com>
2024-02-14 19:21:19 +0900

    Replace navigation items with icons

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -23,6 +23,13 @@ module.exports = defineUserConfig({
 			},
 		],
 		['link', {rel: 'icon', href: '/logo.svg'}],
+		[
+			'link',
+			{
+				rel: 'stylesheet',
+				href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+			},
+		],
 	],
 	theme: defaultTheme({
 		navbar: [
