7396eab6af1c00c10bdabe4e543031f987b49d72
Baku Hashimoto <mail@baku89.com>
2024-02-16 12:25:45 +0900

    Fix Circle.circumcircle

--- src/Circle.ts
+++ src/Circle.ts
@@ -20,14 +20,16 @@ export namespace Circle {
 	export function circumcircle(a: vec2, b: vec2, c: vec2): Circle {
 		const d =
 			2 * (a[0] * (b[1] - c[1]) + b[0] * (c[1] - a[1]) + c[0] * (a[1] - b[1]))
-		const center = vec2.scale(
-			vec2.add(
-				vec2.mul(vec2.add(a, b), vec2.sub(b, c)),
-				vec2.mul(vec2.add(b, c), vec2.sub(c, a)),
-				vec2.mul(vec2.add(c, a), vec2.sub(a, b))
-			),
-			d
-		)
+
+		const A2 = vec2.sqrLen(a)
+		const B2 = vec2.sqrLen(b)
+		const C2 = vec2.sqrLen(c)
+
+		const center: vec2 = [
+			(A2 * (b[1] - c[1]) + B2 * (c[1] - a[1]) + C2 * (a[1] - b[1])) / d,
+			(A2 * (c[0] - b[0]) + B2 * (a[0] - c[0]) + C2 * (b[0] - a[0])) / d,
+		]
+
 		const radius = vec2.dist(center, a)
 		return [center, radius]
 	}
