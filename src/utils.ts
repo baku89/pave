0e8dde1a1f0be5a93a980a417456bf0cc2fc746c
Baku Hashimoto <mail@baku89.com>
2024-02-16 10:20:00 +0900

    Discard the type Code

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import {vec2} from 'linearly'
 
-import {Code, Command} from './Path'
+import {Command} from './Path'
 import {Segment} from './Segment'
 
 export function toFixedSimple(a: number, fractionDigits = 2): string {
@@ -29,7 +29,7 @@ export function memoize<Arg extends object, ReturnType>(
 	}
 }
 
-export function memoizeSegmentFunction<C extends Code, T>(
+export function memoizeSegmentFunction<C extends Command, T>(
 	f: (segment: Segment<C>) => T
 ) {
 	const cache = new WeakMap<vec2, WeakMap<Command, WeakMap<vec2, T>>>()
