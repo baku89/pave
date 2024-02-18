ad1d737d1c7d49e0831b335645e6da013206d197
Baku Hashimoto <mail@baku89.com>
2024-02-18 10:02:08 +0900

    Implement curve-time props for Line segment

--- src/Line.ts
+++ src/Line.ts
@@ -1,5 +1,6 @@
-import {vec2} from 'linearly'
+import {mat2d, vec2} from 'linearly'
 
+import {SegmentLocation} from './Location'
 import {VertexL} from './Path'
 import {Rect} from './Rect'
 import {SegmentL} from './Segment'
@@ -13,9 +14,30 @@ export namespace Line {
 		return Rect.fromPoints(start, end)
 	}
 
-	export function pointAtTime(line: SegmentL, t: number) {
-		const {start, end} = line
-		return vec2.lerp(start, end, t)
+	export function point(line: SegmentL, loc: SegmentLocation): vec2 {
+		const time = toTime(line, loc)
+		return vec2.lerp(line.start, line.end, time)
+	}
+
+	export function derivative(line: SegmentL): vec2 {
+		return vec2.sub(line.end, line.start)
+	}
+
+	export function tangent(line: SegmentL): vec2 {
+		return vec2.normalize(derivative(line))
+	}
+
+	export function normal(line: SegmentL): vec2 {
+		return vec2.rotate(tangent(line), 90)
+	}
+
+	export const curvature = 0
+
+	export function orientation(line: SegmentL, loc: SegmentLocation): mat2d {
+		const p = point(line, loc)
+		const xAxis = tangent(line)
+		const yAxis = vec2.rotate(xAxis, 90)
+		return [...xAxis, ...yAxis, ...p]
 	}
 
 	export function divideAtTimes(line: SegmentL, times: number[]): VertexL[] {
@@ -29,5 +51,17 @@ export namespace Line {
 	 */
 	export function isZero(line: SegmentL) {
 		return vec2.equals(line.start, line.end)
+	}
+
+	export function toTime(line: SegmentL, loc: SegmentLocation): number {
+		if (typeof loc === 'number') {
+			return loc
+		} else if ('time' in loc) {
+			return loc.time
+		} else if ('unit' in loc) {
+			return loc.unit
+		} else {
+			return loc.offset / vec2.distance(line.start, line.end)
+		}
 	}
 }
