fc0240da921d599637081da5b60f4120b6acaaf7
Baku Hashimoto <mail@baku89.com>
2024-03-28 15:32:18 +0900

    Declare normalizeOffset and normalizeIndex

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,18 @@
+import {scalar} from 'linearly'
+
 export function toFixedSimple(a: number, fractionDigits = 2): string {
 	return a
 		.toFixed(fractionDigits)
 		.replace(/(?:\.0+$)|(?:(\.[0-9]*?)0+$)/, '$1')
 		.replace(/^(-?)0\./, '$1.')
+}
+
+export function normalizeOffset(offset: number, max: number) {
+	return scalar.clamp(offset >= 0 ? offset : max + offset, 0, max)
+}
+
+export function normalizeIndex(index: number, length: number) {
+	return scalar.clamp(index >= 0 ? index : length + index, 0, length - 1)
 }
 
 /**
