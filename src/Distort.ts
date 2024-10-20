3fdedfaa896650e41839438bfc382d075a4e7141
Baku Hashimoto <mail@baku89.com>
2024-10-20 10:59:07 +0900

    Add Distort.wave

--- src/Distort.ts
+++ src/Distort.ts
@@ -17,4 +17,37 @@ export namespace Distort {
 			return [...axisX, ...axisY, ...pt]
 		}
 	}
+
+	/**
+	 * Add a sine wave distortion to the given point.
+	 * @param amplitude The amplitude of the wave.
+	 * @param width The wavelength of the wave.
+	 * @param phase The phase of the wave in degrees.
+	 * @param angle The angle of the wave in degrees.
+	 * @param origin The origin of the wave.
+	 * @returns The wave distortion function.
+	 */
+	export function wave(
+		amplitude: number,
+		width: number,
+		phase = 0,
+		angle = 0,
+		origin = vec2.zero
+	): Distort {
+		const xform = mat2d.rotation(angle, origin)
+		const invXform = mat2d.invert(xform) ?? mat2d.id
+
+		return fromPointTransformer((p: vec2) => {
+			const pLocal = vec2.transformMat2d(p, invXform)
+
+			const theta = (pLocal[0] / width + phase / 360) * 2 * Math.PI
+
+			const yOffset = Math.sin(theta) * amplitude
+
+			const pDistortedLocal = vec2.add(pLocal, [0, yOffset])
+			const pDistorted = vec2.transformMat2d(pDistortedLocal, xform)
+
+			return pDistorted
+		})
+	}
 }
